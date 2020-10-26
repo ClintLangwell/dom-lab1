a8d5595b30a289c64aa4d4ce7fe2dac675c7a946
Clint Langwell <clintlangwell@gmail.com>
2020-10-26 17:49:58 -0400

    functionality done through Make Money section

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 "use strict";
+// Vending Machine Code
+let purchaseBtn = document.querySelectorAll(".purchaseBtn");
+let total = 0;
+purchaseBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    let display = document.querySelector(".display");
+    let price = parseFloat(button.getAttribute("data-price"));
+    total += price;
+    display.innerText = `Total Price: $${total.toFixed(2)}`;
+  });
+});
+
+// Make Money Code
+let form = document.querySelector(".form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(form);
+  let howMany = formData.get("howMany");
+  let whichCoinType = formData.get("whichCoin");
+  let coinContainer = document.querySelector(".coin-container");
+
+  for (let i = 0; i < howMany; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", whichCoinType);
+    newDiv.innerText = whichCoinType;
+    newDiv.classList.add("center-text");
+    coinContainer.append(newDiv);
+  }
+  form.reset();
+});
